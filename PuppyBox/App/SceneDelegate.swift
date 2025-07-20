3e40f8316add652b1e42fc8b7115ae97db075133
가현 <102145605+rkgus24@users.noreply.github.com>
2025-07-20 23:54:54 +0900

    add: collectionView 추가 && Layout

--- PuppyBox/App/SceneDelegate.swift
+++ PuppyBox/App/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 //        window.rootViewController = MainTabBarController()
         // window.rootViewController = navi
-        window.rootViewController = UINavigationController(rootViewController: MainTabBarController())
+        window.rootViewController = UINavigationController(rootViewController: LoginViewController())
         window.makeKeyAndVisible()
         self.window = window
     }
--- PuppyBox/Scenes/MovieList/SearchViewController.swift
+++ PuppyBox/Scenes/MovieList/SearchViewController.swift
@@ -8,8 +8,9 @@
 import SnapKit
 import Then
 import UIKit
+import Kingfisher
 
-class SearchViewController: UIViewController, UISearchBarDelegate {
+class SearchViewController: UIViewController, UISearchBarDelegate, UICollectionViewDataSource {
     private let searchBar = UISearchBar().then {
         $0.placeholder = "검색어를 입력하세요"
         $0.searchBarStyle = .minimal
@@ -18,10 +19,17 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
         $0.searchTextField.clearButtonMode = .whileEditing
     }
 
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeLayout()).then {
+        $0.backgroundColor = .white
+        $0.register(MoviePosterCell.self, forCellWithReuseIdentifier: MoviePosterCell.identifier)
+        $0.dataSource = self
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupLayout()
+        setupCollectionViewLayout()
         searchBar.delegate = self
     }
 
@@ -35,6 +43,48 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
         }
     }
 
+    private func makeLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1 / 3),
+            heightDimension: .absolute(152)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(152)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item, item, item]
+        )
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+
+    private func setupCollectionViewLayout() {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(16)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MoviePosterCell.identifier, for: indexPath) as! MoviePosterCell
+        cell.setNumber(nil)
+        return cell
+    }
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print("현재 검색어 :", searchText)
     }
