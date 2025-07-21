b5f5102e7dc171040d77f7b8eeed8c1d1492342e
Yoon-Mac <sryoowk@gmail.com>
2025-07-21 19:31:58 +0900

    Chroe #55 : UIColor에 더보기버튼의 색을 추가하고 연동되게 바꾸었습니다.

--- PuppyBox/Extensions/UIColor+AppColors.swift
+++ PuppyBox/Extensions/UIColor+AppColors.swift
@@ -9,4 +9,5 @@ import UIKit
 
 extension UIColor {
     static let appPrimary: UIColor = .init(red: 229 / 255.0, green: 66 / 255.0, blue: 53 / 255.0, alpha: 1.0)
+    static let appGray: UIColor = .init(white: 197 / 255, alpha: 1)
 }
