36817773d7f53c2595201e3ff366ca28b2b0df0e
Yoon-Mac <sryoowk@gmail.com>
2025-07-21 11:00:59 +0900

    Chore, Add #39 오토레이아웃에 맞추어서 수정

--- PuppyBox/Scenes/MyPage/MovieCell.swift
+++ PuppyBox/Scenes/MyPage/MovieCell.swift
@@ -9,18 +9,26 @@ final class MovieCell: UICollectionViewCell {
     static let identifier = String(describing: MovieCell.self)
 
     private let moviePosterView = UIImageView().then {
-        $0.contentMode = .scaleAspectFill
+        $0.contentMode = .scaleAspectFit
         $0.clipsToBounds = true
+        $0.layer.cornerRadius = 8
+    }
+    
+    private let backgroundPosterView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.layer.cornerRadius = 8
         $0.backgroundColor = .systemGray5
+        $0.clipsToBounds = true
+        $0.alpha = 0.4
     }
 
     private let movieNameLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 18, weight: .medium)
+        $0.font = .plusJakarta(size: 16, weight: .medium)
         $0.textColor = .label
     }
 
     private let dateLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 14, weight: .medium)
+        $0.font = .plusJakarta(size: 12, weight: .medium)
         $0.textColor = .appPrimary
     }
 
@@ -31,6 +39,31 @@ final class MovieCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+       
+        
+        stackView.addArrangedSubview(movieNameLabel)
+        stackView.addArrangedSubview(dateLabel)
+        
+        contentView.addSubview(backgroundPosterView)
+        contentView.addSubview(moviePosterView)
+        contentView.addSubview(stackView)
+        
+        backgroundPosterView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(8)
+            $0.leading.equalToSuperview().inset(16)
+            $0.width.height.equalTo(56)
+        }
+        
+       
+        moviePosterView.snp.makeConstraints {
+            $0.directionalEdges.equalTo(backgroundPosterView)
+        }
+
+        stackView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(moviePosterView.snp.trailing).offset(16)
+        }
     }
 
     @available(*, unavailable)
@@ -39,26 +72,16 @@ final class MovieCell: UICollectionViewCell {
     }
 
     func configure(with movie: MyMovie) {
-        stackView.addArrangedSubview(movieNameLabel)
-        stackView.addArrangedSubview(dateLabel)
-
-        contentView.addSubview(moviePosterView)
-        contentView.addSubview(stackView)
 
         movieNameLabel.text = movie.movieName
         dateLabel.text = movie.screeningDate
+        // Kf downloadImage
         if let url = URL(string: movie.posterImagePath) {
             moviePosterView.kf.setImage(with: url)
+            backgroundPosterView.kf.setImage(with: url)
         }
 
-        moviePosterView.snp.makeConstraints {
-            $0.top.bottom.leading.equalToSuperview()
-            $0.width.equalTo(92)
-        }
+      
 
-        stackView.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.leading.equalTo(moviePosterView.snp.trailing).offset(10)
-        }
     }
 }
