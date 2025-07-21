b3ac44dca11e6c49a1a50890eaf546652f014c5c
Yoon-Mac <sryoowk@gmail.com>
2025-07-21 17:01:36 +0900

    Feat #47 : 코어데이터에서 데이터를 읽어와 마이페이지에 연동기능 완성

--- PuppyBox/Scenes/MyPage/MyPageViewModel.swift
+++ PuppyBox/Scenes/MyPage/MyPageViewModel.swift
@@ -3,79 +3,84 @@ import Foundation
 
 final class MyPageViewModel: ViewModelProtocol {
     enum Action {
-        case fetchUserData
+        case updateUserData
     }
-    
+
     struct State {
         var userData: UserData
     }
-    
+
     private(set) var state: State {
         didSet { onStateChanged?(state) }
     }
-    
-    
-    
+
     var onStateChanged: ((State) -> Void)?
-    
+
     init() {
         @UserSetting(key: UDKey.userId, defaultValue: "")
         var userId: String
-        if let account = CoreDataManager.shared.fetchAccount(userId: userId){
-            self.state = State(userData: makeUserData(from: account))
+        if let account = CoreDataManager.shared.fetchAccount(userId: userId) {
+            state = State(userData: updateUserData(from: account))
         } else {
-            self.state = State(userData:
-                                UserData(
-                                    nickname: "",
-                                    userId: "",
-                                    email: "",
-                                    profileImageUrl: "defaultProfile",
-                                    reservedMovies: [],
-                                    watchedMovies: [])
+            state = State(userData:
+                UserData(
+                    nickname: "",
+                    userId: "",
+                    email: "",
+                    profileImageUrl: "defaultProfile",
+                    reservedMovies: [],
+                    watchedMovies: []
+                )
             )
         }
     }
-    
-    
-    
+
     func action(_ action: Action) {
-        
+        switch action {
+        case .updateUserData:
+            @UserSetting(key: UDKey.userId, defaultValue: "")
+            var userId: String
+            let account = CoreDataManager.shared.fetchAccount(userId: userId)!
+            state = State(userData: updateUserData(from: account))
+        }
     }
-    
-    
 }
 
-private func makeUserData(from account: Account) -> UserData {
-    let watched: [MyMovie]
-    
-    if let movies = account.watchedMovies as? Set<WatchedMovie>{
-        watched = movies.map{
-            MyMovie(
-                movieId: Int($0.movieId),
-                movieName: $0.movieName,
-                posterImagePath: $0.posterImagePath ?? "",
-                screeningDate: DateFormat.dateToString($0.screeningDate),
-            )
-        }
-    } else {
-        watched = []
-    }
-    
+private func updateUserData(from account: Account) -> UserData {
     let reservations: [MyMovie]
-    
-    if let movies = account.reservation as? Set<WatchedMovie>{
-        reservations = movies.map{
+
+    if let movies = account.reservation as? Set<Reservation> {
+        reservations = movies.map {
             MyMovie(
                 movieId: Int($0.movieId),
                 movieName: $0.movieName,
                 posterImagePath: $0.posterImagePath ?? "",
                 screeningDate: DateFormat.dateToString($0.screeningDate),
             )
+        }.sorted {
+            $0.screeningDate < $1.screeningDate
         }
     } else {
         reservations = []
     }
-    
+
+    let watched: [MyMovie]
+
+    if let movies = account.watchedMovies as? Set<WatchedMovie> {
+        watched = movies.map {
+            MyMovie(
+                movieId: Int($0.movieId),
+                movieName: $0.movieName,
+                posterImagePath: $0.posterImagePath ?? "",
+                screeningDate: DateFormat.dateToString($0.screeningDate),
+            )
+        }.sorted {
+            $0.screeningDate > $1.screeningDate
+        }
+    } else {
+        watched = []
+    }
+
     return UserData(
         nickname: account.name,
         userId: account.userId,
