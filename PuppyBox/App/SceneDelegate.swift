25818a95a082bfbdd8178572a6b33a559fab64b7
가현 <102145605+rkgus24@users.noreply.github.com>
2025-07-22 01:05:31 +0900

    add: 앱 실행 시 애니메이션 구현

--- PuppyBox/App/SceneDelegate.swift
+++ PuppyBox/App/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: LoginViewController())
+        window.rootViewController = UINavigationController(rootViewController: SplashViewController())
         window.makeKeyAndVisible()
         self.window = window
         let appearance = UITabBarAppearance()
